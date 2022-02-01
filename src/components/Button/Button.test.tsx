e380de524be4e2e5a4923d989afc3dec1427136f
Gabriel Jodas <70294805+dotdott@users.noreply.github.com>
2022-02-01 13:41:30 -0300

    feat: button component tests functionalities finish

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,5 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Button from "./Button";
+
+const mockFunction = jest.fn();
+
+const mockChildrens = () => {
+  return <p>My beautiful paragraph :)</p>;
+};
 
 describe("Button Component", () => {
   it("should render button component with default blue class when not passed an value in prop", () => {
@@ -8,5 +14,47 @@ describe("Button Component", () => {
     const btnElement = screen.getByTestId("button-test-id");
 
     expect(btnElement).toHaveClass("button _blue");
+  });
+
+  it("should render button with declared className passed by props", () => {
+    render(<Button btnClasses="_red" />);
+
+    const btnElement = screen.getByTestId("button-test-id");
+
+    expect(btnElement).toHaveClass("button _red");
+  });
+
+  it("should execute the onClick mock function", () => {
+    render(<Button btnFunction={mockFunction} />);
+
+    const btnElement = screen.getByTestId("button-test-id");
+
+    fireEvent.click(btnElement);
+
+    expect(mockFunction).toHaveBeenCalled();
+  });
+
+  it("should custom style being passed by props, like opacity 0", () => {
+    render(
+      <Button
+        btnExtraStyles={{
+          opacity: "0.5",
+        }}
+      />
+    );
+
+    const btnElement = screen.getByTestId("button-test-id");
+
+    expect(btnElement).toHaveProperty("style.opacity", "0.5");
+  });
+
+  it("should have rendered the red button color and have a paragraphic with any text", () => {
+    render(<Button btnClasses="_red">{mockChildrens()}</Button>);
+
+    const btnElement = screen.getByTestId("button-test-id");
+    expect(btnElement).toHaveClass("button _red");
+
+    const paraElement = screen.getByText(/My beautiful paragraph/i);
+    expect(paraElement).toBeInTheDocument();
   });
 });
