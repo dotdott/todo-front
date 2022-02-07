a25e683fc0f9f78a671f117fd7b69ab5d84200f4
Gabriel Jodas <70294805+dotdott@users.noreply.github.com>
2022-02-07 15:27:44 -0300

    feat: initiate register page tests and testing if user is redirected when there is an user logged in and trying to access /register url

---
+++ src/views/Register/__tests__/Register.spec.tsx
@@ -0,0 +1,72 @@
+import { createMemoryHistory } from "history";
+import { useSelector } from "react-redux";
+import { Router } from "react-router-dom";
+import { render } from "src/util/test-utils";
+import Register from "..";
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useSelector: jest.fn(),
+}));
+
+afterAll(() => {
+  jest.restoreAllMocks();
+});
+
+describe("trying to access register page when an user is logged in", () => {
+  beforeEach(() => {
+    (useSelector as jest.Mock).mockImplementation(() => {
+      const value = {
+        id: 1,
+      };
+
+      return value;
+    });
+  });
+
+  afterEach(() => {
+    (useSelector as jest.Mock).mockClear();
+  });
+
+  it("should redirect user back when there is an user logged in.", () => {
+    const history = createMemoryHistory();
+    const goBackSpy = jest.spyOn(history, "goBack");
+
+    render(
+      <Router history={history}>
+        <Register />
+      </Router>
+    );
+
+    expect(goBackSpy).toHaveBeenCalled();
+  });
+});
+
+describe("trying to access registedr page when an user is logged in", () => {
+  beforeEach(() => {
+    (useSelector as jest.Mock).mockImplementation(() => {
+      const value = {
+        id: -1,
+      };
+
+      return value;
+    });
+  });
+
+  afterEach(() => {
+    (useSelector as jest.Mock).mockClear();
+  });
+
+  it("should redirect user back when there is an user logged in.", () => {
+    const history = createMemoryHistory();
+    const goBackSpy = jest.spyOn(history, "goBack");
+
+    render(
+      <Router history={history}>
+        <Register />
+      </Router>
+    );
+
+    expect(goBackSpy).toHaveBeenCalled();
+  });
+});
