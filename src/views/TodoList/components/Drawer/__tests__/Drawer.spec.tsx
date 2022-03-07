91be5a7f2a0c2bb6570ae63190f1e14634e48f93
Gabriel Jodas <70294805+dotdott@users.noreply.github.com>
2022-03-07 13:41:30 -0300

    test: drawer should rendering only thoses tasks that has the completed flag true

--- src/views/TodoList/components/Drawer/__tests__/Drawer.spec.tsx
+++ src/views/TodoList/components/Drawer/__tests__/Drawer.spec.tsx
@@ -1,4 +1,10 @@
-import { render, waitFor, fireEvent } from "src/util/test-utils";
+import {
+  render,
+  waitFor,
+  fireEvent,
+  spySelector,
+  mockTodos,
+} from "src/util/test-utils";
 import Drawer from "..";
 
 const openDrawer = jest.fn(() => true);
@@ -29,6 +35,24 @@ describe("Drawer component", () => {
     waitFor(() => {
       fireEvent.click(getByTestId("toggle-drawer-test"));
       expect(getByText(/Completados/i)).not.toBeInTheDocument();
+    });
+  });
+});
+
+describe("Drawer with mocked redux data", () => {
+  spySelector(mockTodos);
+
+  it("should only show tasks that is completed", () => {
+    const { getByText } = render(
+      <Drawer
+        openDrawer={openDrawer()}
+        handleToggleDrawer={handleToggleDrawer}
+      />
+    );
+
+    waitFor(() => {
+      expect(getByText(mockTodos[0].task)).toBeInTheDocument();
+      expect(getByText(mockTodos[1].task)).not.toBeInTheDocument();
     });
   });
 });
