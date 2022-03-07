91348ac62474ad406f4ab8d668dbbcc991c90e4b
Gabriel Jodas <70294805+dotdott@users.noreply.github.com>
2022-03-07 15:39:08 -0300

    refactor(test-createTodoModal): creating setup function to start rendering component

--- src/views/TodoList/components/CreateTodoModal/__tests__/CreateTodoModal.spec.tsx
+++ src/views/TodoList/components/CreateTodoModal/__tests__/CreateTodoModal.spec.tsx
@@ -1,19 +1,25 @@
 import { render } from "src/util/test-utils";
 import CreateTodoModal from "..";
 
-const showModal = jest.fn(() => true);
-const closeModal = showModal.mockReturnValue(false);
+const showModal = jest.fn();
+const closeModal = showModal.mockImplementationOnce(() => false);
 const modalRef = jest.fn();
+
+const setup = () => {
+  const { getByText, ...rest } = render(
+    <CreateTodoModal
+      show={showModal.mockImplementationOnce(() => true)()}
+      handleClose={closeModal}
+      modalRef={modalRef}
+    />
+  );
+
+  return { getByText, ...rest };
+};
 
 describe("Modal should open display the create modal layout", () => {
   it("should display 'create' texts fields in modal when there is no todo selected", () => {
-    const { getByText } = render(
-      <CreateTodoModal
-        show={showModal()}
-        handleClose={closeModal}
-        modalRef={modalRef}
-      />
-    );
+    const { getByText } = setup();
 
     expect(getByText(/Crie uma nova tarefa!/i)).toBeInTheDocument();
     expect(getByText(/CRIAR NOVA TAREFA/i)).toBeInTheDocument();
